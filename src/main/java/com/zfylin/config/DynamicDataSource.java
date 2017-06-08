a8d55f38bbe01a3c13a0c4053b9a2d5f33dea124
zfylin <zfylin2014@163.com>
2017-06-08 18:21:41 +0800

    使用注解自动切换数据源

--- src/main/java/com/zfylin/config/DynamicDataSource.java
+++ src/main/java/com/zfylin/config/DynamicDataSource.java
@@ -3,9 +3,19 @@ package com.zfylin.config;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 /**
- * 动态数据源（需要继承AbstractRoutingDataSource）
+ * 动态数据源
+ *
+ * @author zfylin
+ * @version 2017/6/6
  */
 public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    /**
+     * determineCurrentLookupKey方法取得一个字符串，
+     * 该字符串将与配置文件中的相应字符串进行匹配以定位数据源，配置文件，即applicationContext.xml文件中需要要如下代码：(non-Javadoc)
+     *
+     * @see org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource#determineCurrentLookupKey()
+     */
     protected Object determineCurrentLookupKey() {
         return DatabaseContextHolder.getDatabaseType();
     }
